c75f5331c99d2eacd8bdbd5db3f43257c9d5b1f7
Brant Strand <bstrand@gmail.com>
2015-01-23 13:34:18 -0800

    Initial, non-working commit

---
+++ pcap2avro.py
@@ -0,0 +1,112 @@
+__author__ = 'bstrand'
+
+import sys
+import socket
+import argparse
+import dpkt
+
+import avro.schema
+from avro.datafile import DataFileWriter
+from avro.io import DatumWriter
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='pcap2avro - Serialize IP packets from pcap files into Avro format')
+    parser.add_argument('pcap_files', metavar='file', nargs='+',
+                        help='pcap file(s) to serialize')
+    parser.add_argument('output_file', metavar='out', nargs='+',
+                        help='Output file; defaults to <source filename>.avro')
+    parser.add_argument("--debug", help="Write debug output.", action="store_true")
+    args = parser.parse_args()
+    if args.debug:
+        print "Args parsed."
+        print "Input files: "
+        for filename in args.pcap_files:
+            print "\t", filename
+    return args
+
+
+def proto_has_port(p):
+    return p==6 or p==17
+
+def proto_id_to_name(p):
+    if p==6:
+        return 'tcp'
+    elif p == 17:
+        return 'udp'
+    else:
+        return '???'
+
+def main():
+    # args = parse_args()
+    infile = "/Users/bstrand/insight/pcaps/live/live-20150123.pcap"
+    outfile = "/Users/bstrand/insight/pcaps/live/live-20150123.pcap"
+
+    try:
+        schema = avro.schema.parse(open("/Users/bstrand/insight/pcap2avro/tcp.avsc").read())
+    except:
+        print "Failed to parse Avro schema file"
+        sys.exit()
+
+    try:
+        f = open(infile, 'rb')
+    except:
+        print "Failed to open " + infile
+        sys.exit()
+
+    try:
+        pcap = dpkt.pcap.Reader(f)
+    except:
+        print "Failed to parse " + infile
+        sys.exit()
+
+    try:
+        writer = DataFileWriter(open(outfile, "w"), DatumWriter(), schema)
+    except:
+        print "Failed to open Avro output file %s" % outfile
+        sys.exit()
+
+    for ts, buf in pcap:
+        try:
+            eth = dpkt.ethernet.Ethernet(buf)
+            if eth.type != dpkt.ethernet.ETH_TYPE_IP:
+                continue
+        except:
+            continue
+
+        ip = eth.data
+        src = socket.inet_ntoa(ip.src)
+        dst = socket.inet_ntoa(ip.dst)
+        # TODO Temporary limitation to IPv4 TCP packets
+        if (ip.p != 6):
+            continue
+
+        tcp = ip.data
+        sport = str(tcp.sport)
+        dport = str(tcp.dport)
+
+        proto = proto_id_to_name(ip.p)
+        packetlength = ip.len-ip.hl*4
+
+        print "%s : %d -> %s : %d, proto: %s, len: %d, seq: %d, ack: %d, flags 0x%02x, win %d" \
+              % (src, tcp.sport, dst, tcp.dport, proto, packetlength, tcp.seq, tcp.ack, tcp.flags, tcp.win)
+
+        writer.append({
+            "timestamp": str(ts),
+            "ip_TTL": str(ip.ttl),
+            "ip_proto": proto,
+            "ip_src": src,
+            "ip_dst": dst,
+            "ip_pkt_len": ip.len,
+            "tcp_src_port": tcp.sport,
+            "tcp_dst_port": tcp.dport,
+            "tcp_seq_num": tcp.seq,
+            "tcp_ack_num": tcp.ack,
+            "tcp_flags": tcp.flags,
+            "tcp_window": tcp.win
+        })
+
+    writer.close()
+
+
+if __name__ == '__main__':
+    main()
