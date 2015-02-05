0d585785b53939ae49eb9011569f89c52e4d6acc
Brant Strand <bstrand@gmail.com>
2015-02-05 15:43:08 -0800

    UDP parsing

--- pcap2avro.py
+++ pcap2avro.py
@@ -1,10 +1,14 @@
 __author__ = 'bstrand'
 
+# TODO convert to class
+# TODO refactor for config to replace hardcoded settings
+
 import sys
 import socket
 import argparse
 import dpkt
 from datetime import datetime
+from collections import Counter
 
 import avro.schema
 from avro.datafile import DataFileWriter
@@ -38,12 +42,14 @@ def proto_has_port(p):
     return p==6 or p==17
 
 def proto_id_to_name(p):
-    if p==6:
-        return 'tcp'
+    if p==2:
+        return 'ICMP'
+    elif p==6:
+        return 'TCP'
     elif p == 17:
-        return 'udp'
+        return 'UDP'
     else:
-        return '???'
+        return 'other'
 
 
 def init_kafka(endpoint):
@@ -91,8 +97,8 @@ def ingest_file(pcap_file):
 
     # Initialize
     schema = read_avro_schema(avro_schema_file)
-
     pcap = read_pcap(pcap_file)
+    packet_count = Counter(['total','IP','IPv6','dropped','TCP','UDP','ICMP','error'])
 
    #producer = init_kafka(kafka_endpoint)
 
@@ -104,18 +110,31 @@ def ingest_file(pcap_file):
         sys.exit()
 
     for ts, buf in pcap:
+        packet_count['total'] += 1
+
+        # Initial packet parse, drop anything but IP frames
         try:
             eth = dpkt.ethernet.Ethernet(buf)
             if eth.type != dpkt.ethernet.ETH_TYPE_IP:
+                packet_count['dropped'] += 1
                 continue
         except:
+            packet_count['error'] += 1
             continue
 
+        # Parse IP packet; drop anything IPv4/{ICMP,TCP,UDP}
         ip = eth.data
-        # TODO limited to IPv4 TCP packets
-        if (ip.p != 6):
+        if (ip.p not in [2,6,17]):
+            if (ip.p == 41):
+                packet_count['IPv6'] += 1
+            packet_count['dropped'] += 1
             continue
-        tcp = ip.data
+        packet_count['IP'] += 1
+
+        # Init protocol data
+        tcp_data = None
+        icmp_data = None
+        udp_data = None
 
         if(args.debug):
             # Do some conversions now
@@ -123,20 +142,41 @@ def ingest_file(pcap_file):
             dst = socket.inet_ntoa(ip.dst)
             proto = proto_id_to_name(ip.p)
             packetlength = ip.len-ip.hl*4
-
-            # Debug output
             humanTS = datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S.%f')
-            pktStr =  "%s | %s  %s : %d -> %s : %d, len: %d, seq: %d, ack: %d, flags 0x%02x, win %d" \
-                  % (humanTS, proto.upper(), src, tcp.sport, dst, tcp.dport, packetlength, tcp.seq, tcp.ack, tcp.flags, tcp.win )
-            print pktStr
 
-        tcp_data = {}
-        tcp_data['src_port'] = tcp.sport
-        tcp_data['dst_port'] = tcp.dport
-        tcp_data['seq_num'] = tcp.seq
-        tcp_data['ack_num'] = tcp.ack
-        tcp_data['flags'] = tcp.flags
-        tcp_data['window'] = tcp.win
+        if (ip.p == 2):
+            packet_count['ICMP'] += 1
+            # TODO
+        elif (ip.p == 6):
+            packet_count['TCP'] += 1
+            tcp = ip.data
+
+            if(args.debug):
+                pktStr =  "%d | %s | %s | %s : %d -> %s : %d, len: %d, seq: %d, ack: %d, flags 0x%02x, win %d" \
+                      % (packet_count['total'], humanTS, proto, src, tcp.sport, dst, tcp.dport, packetlength, tcp.seq, tcp.ack, tcp.flags, tcp.win )
+                print pktStr
+
+            tcp_data = {}
+            tcp_data['src_port'] = tcp.sport
+            tcp_data['dst_port'] = tcp.dport
+            tcp_data['seq_num']  = tcp.seq
+            tcp_data['ack_num']  = tcp.ack
+            tcp_data['flags']    = tcp.flags
+            tcp_data['window']   = tcp.win
+
+        elif (ip.p == 17):
+            packet_count['UDP'] += 1
+            udp = ip.data
+
+            if(args.debug):
+                pktStr =  "%d | %s | %s | %s : %d -> %s : %d, len: %d" \
+                      % (packet_count['total'], humanTS, proto, src, udp.sport, dst, udp.dport, udp.ulen)
+                print pktStr
+
+            udp_data = {}
+            udp_data['src_port'] = udp.sport
+            udp_data['dst_port'] = udp.dport
+            udp_data['length']   = udp.ulen
 
         writer.append({
             "ts": ts,
@@ -147,7 +187,9 @@ def ingest_file(pcap_file):
             "dst_addr": socket.inet_ntoa(ip.dst),
             "length": ip.len,
 
-            "tcp": tcp_data
+            "tcp":  tcp_data,
+            "udp":  udp_data,
+            "icmp": icmp_data
         })
 
         #producer.send_messages("pcap_bin_test", pktStr)
